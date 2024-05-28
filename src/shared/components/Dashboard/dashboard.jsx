59a42bd5357d2aabfd14ce3791d7135c3f0f549e
kalaiselvan37 <kalaiselvan.tcz@gmail.com>
2024-05-28 09:22:39 +0530

    Dashboard changes-28-05

--- src/shared/components/Dashboard/dashboard.jsx
+++ src/shared/components/Dashboard/dashboard.jsx
@@ -9,19 +9,19 @@ export default function Dashboard(props) {
     return (
       <>
     <div className="grid justify-center h-[450px] grid-cols-1 gap-3 lg:grid-cols-3">
-      <div className='transition-transform duration-300 ease-in-out bg-white shadow-md rounded-xl hover:scale-105'>
+      <div className='bg-white shadow-md rounded-xl'>
       <div className="flex justify-center rounded-xl">
       <div className="p-5 lg:h-[120px] lg:w-[380px]">
-        <h1 className="p-2 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Total Users</h1>
+        <h1 className="p-1 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Total Users</h1>
         <Chart type="pie" data={chartData} options={chartOptions} className="" />
       </div>
       </div>
       </div>
 
-      <div className='transition-transform duration-300 ease-in-out bg-white shadow-md rounded-xl hover:scale-105'>
+      <div className='bg-white shadow-md rounded-xl'>
       <div className="flex justify-center rounded-xl">
       <div className="p-5 lg:h-[250px] lg:w-[400px]">
-        <h1 className="p-2 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Productivity Data</h1>
+        <h1 className="p-1 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Productivity Data</h1>
         <Chart type="doughnut" data={productivitybarchartData} options={productivitybarchartOptions} className="" />
       </div>
       </div>
@@ -29,10 +29,10 @@ export default function Dashboard(props) {
       
     
 
-      <div className='transition-transform duration-300 ease-in-out bg-white shadow-md rounded-xl hover:scale-105'>
+      <div className='bg-white shadow-md rounded-xl'>
       <div className="flex justify-center rounded-xl">
       <div className="p-5 lg:h-[120px] lg:w-[380px]">
-        <h1 className="p-2 text-xl font-semibold text-white bg-purple-400 w-fit rounded-xl">Allocated and Unallocated</h1>
+        <h1 className="p-1 text-xl font-semibold text-white bg-purple-400 w-fit rounded-xl">Allocated and Unallocated</h1>
         <Chart type="pie" data={allocationchartData} options={allocationchartOptions} className="" />
       </div>
       </div>
@@ -42,16 +42,16 @@ export default function Dashboard(props) {
       
 
       <div className="grid justify-center grid-cols-1 gap-3 mt-5 lg:grid-cols-2">
-      <div className='transition-transform duration-300 ease-in-out bg-white shadow-md rounded-xl hover:scale-105'>
+      <div className='bg-white shadow-md rounded-xl'>
       <div className="p-5">
-        <h1 className="p-2 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Telecaller Leads</h1>
+        <h1 className="p-1 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Telecaller Leads</h1>
         <Chart type="bar" data={barchartData} options={barchartOptions} className="" />
       </div>
       </div>
 
-      <div className='transition-transform duration-300 ease-in-out bg-white shadow-md rounded-xl hover:scale-105 '>
+      <div className='bg-white shadow-md rounded-xl'>
       <div className="p-5">
-        <h1 className="p-2 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Products Count</h1>
+        <h1 className="p-1 text-xl font-semibold text-white bg-purple-400 rounded-xl w-fit">Products Count</h1>
         <Chart type="bar" data={procuctsbarchartData} options={productsbarchartOptions} className="" />
       </div>
       </div>
